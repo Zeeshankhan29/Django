ca89b2c3c887d2805422fe76359e7c779fd99b85
Zeeshankhan29 <zeeshankhan29khan@gmail.com>
2023-04-04 16:02:27 +0530

    sql scripts

--- mainapp/firstapp/models.py
+++ mainapp/firstapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class iNeuron(models.Model):
+    course_name = models.CharField(max_length=30),
+    course_id = models.IntegerField(),
+    student_name = models.CharField(max_length=20)
