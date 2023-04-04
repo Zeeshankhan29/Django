d871212fdfed94f00a0ce079ca86620c04a33184
Zeeshankhan29 <zeeshankhan29khan@gmail.com>
2023-04-04 16:02:36 +0530

    settings update

--- mainapp/mainapp/settings.py
+++ mainapp/mainapp/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = "mainapp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django3",
+        "USER": "root",
+        "PASSWORD": "snzk@#1329",
+        "HOST": 'localhost',
+        "PORT": 3306,
     }
 }
 
